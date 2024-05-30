ec92cbd7fb935f9dd155a05dba6e84a2d95744f9
mieum12 <12__26@naver.com>
2024-05-30 16:24:50 +0900

    게시글 상세페이지 - 댓글 레이아웃 구현

--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,7 +1,28 @@
-export default function SinglePage() {
+import BackButton from '@/app/(afterLogin)/_components/BackButton';
+import Post from '@/app/(afterLogin)/_components/Post';
+import style from './singlePost.module.css';
+import CommentForm from './_components/CommentForm';
+export default function SinglePost() {
   return (
-    <main>
-      게시글 상세
-    </main>
+    <div className={style.main}>
+      <div className={style.header}>
+        <BackButton/>
+        <h3 className={style.headerTitle}>게시하기</h3>
+      </div>
+      <Post />
+      <CommentForm />
+      <div>
+        {/* 여기는 답글들 보여주는 곳 */}
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
   )
 }
