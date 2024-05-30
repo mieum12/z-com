1e8bdbdec0ef08f6c63fa7c88ceb97a2faba47b6
mieum12 <12__26@naver.com>
2024-05-30 13:33:46 +0900

    context API 적용

--- src/app/(afterLogin)/home/_components/TabProvider.tsx
+++ src/app/(afterLogin)/home/_components/TabProvider.tsx
@@ -2,12 +2,15 @@
 
 import {createContext, ReactNode, useState} from "react";
 
+// context API 사용으로 상태 관리
+
 export const TabContext = createContext({
   tab: 'rec',
   setTab: (value: 'rec' | 'fol') => {},
 });
 
 type Props = { children: ReactNode };
+
 export default function TabProvider({ children }: Props) {
   const [tab, setTab] = useState('rec');
 
