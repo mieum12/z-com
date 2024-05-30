28930428b8efc0d8c0fc8fb9b2dbc67c9420e4f5
mieum12 <12__26@naver.com>
2024-05-30 15:31:10 +0900

    profile 페이지 구현

--- src/app/(afterLogin)/[username]/page.tsx
+++ src/app/(afterLogin)/[username]/page.tsx
@@ -1,8 +1,46 @@
+import Post from '../_components/Post';
+import style from './profile.module.css';
 
 export default function Profile() {
+  const user = {
+    id: 'e-on',
+    nickname: '이온',
+    image: '/profile.jpg'
+  };
+
   return (
-    <main>
-      프로필페이지
+    <main className={style.main}>
+      <div className={style.header}>
+        {/* <BackButton /> */}
+        <button className={style.backButton}>
+      <svg width={24} viewBox="0 0 24 24" aria-hidden="true"
+           className="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03">
+        <g>
+          <path d="M7.414 13l5.043 5.04-1.414 1.42L3.586 12l7.457-7.46 1.414 1.42L7.414 11H21v2H7.414z"></path>
+        </g>
+      </svg>
+    </button>
+
+        <h3 className={style.headerTitle}>{user.nickname}</h3>
+      </div>
+      <div className={style.userZone}>
+        <div className={style.userImage}>
+          <img src={user.image} alt={user.id}/>
+        </div>
+        <div className={style.userName}>
+          <div>{user.nickname}</div>
+          <div>@{user.id}</div>
+        </div>
+        <button className={style.followButton}>팔로우</button>
+      </div>
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
     </main>
   )
 }
